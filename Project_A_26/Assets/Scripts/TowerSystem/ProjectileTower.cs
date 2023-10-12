ff17724b869f824ea8bfd29cf1e56405b5f98fa2
mataltw04 <20040518koko@gmail.com>
2023-10-12 11:53:22 +0900

    타워 타겟 발사까지

---
+++ Project_A_26/Assets/Scripts/TowerSystem/ProjectileTower.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTower : MonoBehaviour
+{
+    private Tower thisTower;                    //Ÿ�� ���۳�Ʈ�� �ִ� ������Ʈ 
+
+    public GameObject projectile;               //�߻�ü ����
+    public Transform firePoint;                 //�߻� ��ġ ����
+    public float timeBetweenShots = 1f;         //�߻� ����
+    private float shotCounter;                  //�ð� ����
+
+    private Transform target;                   //��ǥ Ÿ�� ����
+    public Transform launcherModel;             //�� ȸ�� ��Ű�����ؼ� 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTower = GetComponent<Tower>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            launcherModel.rotation = Quaternion.Slerp(launcherModel.rotation, Quaternion.LookRotation(target.position - transform.position), 5f * Time.deltaTime);
+
+            launcherModel.rotation = Quaternion.Euler(0f, launcherModel.rotation.eulerAngles.y, 0f);
+        }
+
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0 && target != null)
+        {
+            shotCounter = thisTower.fireRate;               //������Ʈ�� Tower ���۳�Ʈ���� ������ FireRate���� �ð��� �����ͼ� �Է�
+            firePoint.LookAt(target);                       //�Ѿ� �߻�� Target�� �ٶ󺻴�.
+            Instantiate(projectile, firePoint.position, firePoint.rotation);    //�Ѿ��� �����Ѵ�. 
+        }
+
+        if (thisTower.enemiesUpdate)         //���� �迭 LIst�� ����������
+        {
+            if (thisTower.enemiesInRange.Count > 0)
+            {
+                float minDistance = thisTower.range + 1;
+
+                foreach (EnemyController enemy in thisTower.enemiesInRange)
+                {
+                    if (enemy != null)
+                    {
+                        float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance; //���� ����� �Ÿ��� ���� 
+                            target = enemy.transform;
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                target = null;
+            }
+        }
+    }
+}
---
+++ Project_A_26/Assets/Scripts/TowerSystem/Tower.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float range = 3.0f;                  //���� ����
+    public float fireRate = 1.0f;               //���� �ӵ�
+
+    public LayerMask isEnemy;                   //���̸� ���ؼ� �� ĳ���� ���� 
+
+    public Collider[] colliderInRange;          //���� ���� �ȿ� ������ Collider �迭 
+
+    public List<EnemyController> enemiesInRange = new List<EnemyController>();  //Collider �迭�� ���ؼ� ���� EnemyController List 
+
+    public float checkCounter;
+    public float checkTime = 0.2f;              //���� ���� �ٽ� �����ϴ� �ð�
+
+    public bool enemiesUpdate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkCounter = checkTime;               //0.2�ʸ� Counter�� �Է�
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemiesUpdate = false;
+
+        checkCounter -= Time.deltaTime;         //Counter�� ������ �ð��� ���� ���Ѽ� �ֱ������� üũ�ϰ� ����
+
+        if (checkCounter <= 0)
+        {
+            checkCounter = checkTime;
+
+            colliderInRange = Physics.OverlapSphere(transform.position, range, isEnemy);    //�ڽ��� ��ġ, ����, ���̾� 
+
+            enemiesInRange.Clear();             //List�� �ִ°� �ʱ�ȭ
+
+            foreach (Collider col in colliderInRange)                       //colliderInRange �迭���� 
+            {
+                enemiesInRange.Add(col.GetComponent<EnemyController>());    //EnemyController�� �޾ƿͼ� List�� �ִ´�.
+            }
+
+            enemiesUpdate = true;
+        }
+
+    }
+}
